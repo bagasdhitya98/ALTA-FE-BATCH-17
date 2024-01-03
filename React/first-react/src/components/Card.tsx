9bfb568fe47d700b31a8e08b0f2128ab42415bf6
bagasdhitya98 <bagas.dhitya@alterra.id>
2024-01-03 20:58:43 +0700

    add: create modal for get by id

--- React/first-react/src/components/Card.tsx
+++ React/first-react/src/components/Card.tsx
@@ -1,27 +1,30 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 interface CardProps {
-  image: string;
   title: string;
-  content: string;
   price: string;
+  onClick: React.MouseEventHandler;
 }
 
 class Card extends Component<CardProps> {
   render() {
-    const { image, title, content, price } = this.props;
+    const { title, price, onClick } = this.props;
 
     return (
-      <div className="w-96 h-full rounded-lg shadow-md flex">
-        <img src={image} className="h-full w-1/2 rounded-l-lg" />
+      <div className="w-full h-full rounded-lg shadow-md flex">
         <div className="flex flex-col p-3 gap-y-5">
           <div>
             <p className="font-bold">{title}</p>
-            <p className="text-sm">{content}</p>
           </div>
           <div>
             <p className="text-sm font-bold">Harga : {price}</p>
           </div>
+          <button
+            onClick={onClick}
+            className="w-full h-10 p-3 text-white bg-blue-500 hover:bg-blue-600 rounded-md flex justify-center items-center"
+          >
+            Details
+          </button>
         </div>
       </div>
     );
--- React/first-react/src/pages/product/index.tsx
+++ React/first-react/src/pages/product/index.tsx
@@ -1,17 +1,90 @@
 import { Component } from "react";
 import { withRouter } from "../../withRouter";
+import axios from "axios";
+
+import Card from "../../components/Card";
+import Modal from "../../components/Modal";
+import Layout from "../../components/Layout";
 
 interface ProductProps {
   location?: any;
 }
 
-export class Product extends Component<ProductProps> {
-  render() {
-    const { location } = this.props;
-    console.log("result of passing username : ", location?.state?.username);
-    console.log("result of passing password : ", location?.state?.password);
+interface ProductState {
+  data?: [];
+  content?: any;
+  isOpen?: boolean;
+}
 
-    return <div>Product</div>;
+export class Product extends Component<ProductProps, ProductState> {
+  state = {
+    data: [],
+    content: null,
+    isOpen: false,
+  };
+
+  getProducts() {
+    axios
+      .get("https://65842cef4d1ee97c6bcf27a5.mockapi.io/product")
+      .then((response) => {
+        this.setState({ data: response?.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  getProductById(id: string) {
+    axios
+      .get(`https://65842cef4d1ee97c6bcf27a5.mockapi.io/product/${id}`)
+      .then((response) => {
+        this.setState({ isOpen: true });
+        this.setState({ content: response?.data });
+        console.log("get by id : ", response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount(): void {
+    this.getProducts();
+  }
+
+  render() {
+    const { data, isOpen, content } = this.state;
+    return (
+      <Layout>
+        <div className="grid grid-cols-5 gap-5">
+          {data ? (
+            data?.map((item: any, index: any) => {
+              return (
+                <Card
+                  key={index}
+                  title={item?.product_name}
+                  price={item?.product_price}
+                  onClick={() => this.getProductById(item?.id)}
+                />
+              );
+            })
+          ) : (
+            <div className="text-center font-semibold text-xl text-blue-500">
+              Loading ...
+            </div>
+          )}
+        </div>
+        {isOpen === true ? (
+          <Modal onClick={() => this.setState({ isOpen: false })}>
+            <div className="p-5">
+              <p className="font-semibold text-lg">{content?.product_name}</p>
+              <p className="font-medium">{content?.product_description}</p>
+            </div>
+          </Modal>
+        ) : (
+          <></>
+        )}
+      </Layout>
+    );
   }
 }
 
