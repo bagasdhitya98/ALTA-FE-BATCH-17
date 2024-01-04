dcfb8c13d4dc5f0fcf0bde4f0de0df58e04d72d1
bagasdhitya98 <bagas.dhitya@alterra.id>
2024-01-04 19:26:38 +0700

    add: pages

--- React/react-hooks/src/App.tsx
+++ React/react-hooks/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Detail from "./pages/detail";
+import News from "./pages/news";
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Login />} path="/auth/login" />
+        <Route element={<Register />} path="/auth/register" />
+        <Route element={<News />} path="/" />
+        <Route element={<Detail />} path="/detail" />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
