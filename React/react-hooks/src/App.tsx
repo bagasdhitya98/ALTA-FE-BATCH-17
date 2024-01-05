ca791770d3c16c4d34409b3319d4d8060b617b60
bagasdhitya98 <bagas.dhitya@alterra.id>
2024-01-05 18:54:16 +0700

    add: add page and rerouting

--- React/react-hooks/src/App.tsx
+++ React/react-hooks/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Detail from "./pages/detail";
+import Count from "./pages/count";
 import News from "./pages/news";
 
 const App = () => {
@@ -11,8 +12,9 @@ const App = () => {
       <Routes>
         <Route element={<Login />} path="/auth/login" />
         <Route element={<Register />} path="/auth/register" />
-        <Route element={<News />} path="/" />
-        <Route element={<Detail />} path="/detail" />
+        <Route element={<Count />} path="/count" />
+        <Route element={<News />} path="/news" />
+        <Route element={<Detail />} path="/news/detail" />
       </Routes>
     </BrowserRouter>
   );
--- React/react-hooks/src/pages/news/index.tsx
+++ React/react-hooks/src/pages/news/index.tsx
@@ -1,44 +1,5 @@
-import { useState, useEffect } from "react";
-
-import Button from "../../components/Button";
-import Input from "../../components/Input";
-
 const News = () => {
-  const [count, setCount] = useState<number>(0);
-  const [mode, setMode] = useState<boolean>(false);
-
-  useEffect(() => {
-    // fase mounting
-    console.log("Component Did Mount");
-
-    // fase unmounting
-    return () => {
-      console.log("Unmounting");
-    };
-
-    // jika ditambahkan state, maka akan berfungsi selayaknya componentDidUpdate
-  }, [count]);
-
-  return (
-    <section
-      className={`flex flex-col justify-center items-center w-screen h-full ${
-        !mode ? "bg-white h-screen" : "bg-slate-700 h-screen"
-      }`}
-    >
-      <div className="w-30 space-x-5 flex">
-        <Button id="increment" title="+" onClick={() => setCount(count + 1)} />
-        <Input
-          id="value-count"
-          value={count}
-          onChange={(e: any) => setCount(e.target.value)}
-        />
-        <Button id="decrement" title="-" onClick={() => setCount(count - 1)} />
-      </div>
-      <div className="">
-        <Button id="mode" title="Mode" onClick={() => setMode(!mode)} />
-      </div>
-    </section>
-  );
+  return <div>News</div>;
 };
 
 export default News;
